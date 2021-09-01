45dba2cf5bd2ca64eeeaa50b018842125ee86679
anvil1004824 <anvil1004824@gmail.com>
2021-09-01 22:12:00 +0900

    Added video cover

--- js/index.js
+++ js/index.js
@@ -1,19 +1,29 @@
 const video = document.getElementById("semicolon-video");
 const cancel = document.getElementById("cancel");
+const videoCover = document.getElementById("video-cover");
 
 function videoClicked() {
   video.classList.add("pop-up");
   video.classList.remove("pop-down");
+  videoCover.classList.add("fadein");
+  videoCover.classList.remove("fadeout");
   cancel.classList.add("cancel-appear");
+  videoCover.classList.remove("hidden");
 }
 
 function cancelClicked() {
   video.classList.remove("pop-up");
   video.classList.add("pop-down");
+  videoCover.classList.remove("fadein");
+  videoCover.classList.add("fadeout");
   cancel.classList.remove("cancel-appear");
   video.pause();
   video.currentTime = 0;
+  setTimeout(() => {
+    videoCover.classList.add("hidden");
+  }, 500);
 }
 
 video.addEventListener("click", videoClicked);
+videoCover.addEventListener("click", cancelClicked);
 cancel.addEventListener("click", cancelClicked);
